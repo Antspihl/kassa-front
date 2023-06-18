507d0ac1d2b1997c90d541917fcd98ae4f09059c
inpihl <antspihl@gmail.com>
2023-06-19 00:48:46 +0300

    Väga aeglaselt loeb failid sisse

--- kassa-front/src/components/Kassa.vue
+++ kassa-front/src/components/Kassa.vue
@@ -1,25 +1,28 @@
 <script setup>
 import {reactive} from 'vue'
 import {useVuelidate} from '@vuelidate/core'
-import {required, alpha, numeric, minValue} from '@vuelidate/validators'
+import {required, numeric, minValue} from '@vuelidate/validators'
 
-const initialState = {
+const Order = {
   name: "",
   drink: "",
   amount: null,
 }
 
 const state = reactive({
-  ...initialState,
+  ...Order,
 })
 
+
 const postUrl = "http://localhost:3000/orders"
-const names = []
-const drinks = []
+let nameLoading = false
+let drinkLoading = false
+let names = [];
+let drinks = [];
 
 const rules = {
-  name: {required, alpha},
-  drink: {required, alpha},
+  name: {required},
+  drink: {required},
   amount: {required, numeric, minValue: minValue(1)},
   names: {required},
   drinks: {required},
@@ -27,10 +30,38 @@ const rules = {
 
 const v$ = useVuelidate(rules, state)
 
+function loadNames() {
+  nameLoading = true
+  const namesFile = document.querySelector('input[type=file]').files[0]
+  const namesReader = new FileReader()
+  namesReader.readAsText(namesFile, "UTF-8")
+  namesReader.onload = function (evt) {
+    names = evt.target.result.split(",")
+  }
+  namesReader.onerror = function () {
+    names = ["Vigane fail"]
+  }
+  nameLoading = false
+}
+
+function loadDrinks() {
+  drinkLoading = true
+  const drinksFile = document.querySelector('input[type=file]').files[0]
+  const drinksReader = new FileReader()
+  drinksReader.readAsText(drinksFile, "UTF-8")
+  drinksReader.onload = function (evt) {
+    drinks = evt.target.result.split(",")
+  }
+  drinksReader.onerror = function (evt) {
+    drinks = ["Vigane fail"]
+  }
+  drinkLoading = false
+}
+
 function clear() {
   v$.value.$reset()
 
-  for (const [key, value] of Object.entries(initialState)) {
+  for (const [key, value] of Object.entries(Order)) {
     state[key] = value
   }
 }
@@ -43,7 +74,6 @@ function clear() {
         v-model="state.name"
         :items="names"
         :error-messages="v$.name.$errors.map(e => e.$message)"
-        :counter="10"
         label="Nimi"
         required
         @input="v$.name.$touch"
@@ -84,17 +114,23 @@ function clear() {
       chips
       class="mt-8"
       accept=".txt"
-      label="Lae nimede fail üles(.txt fail: nimi1, nimi2, nimi3)"
+      label="Lae nimede fail üles(.txt fail: nimi1,nimi2,nimi3)"
     ></v-file-input>
-    <v-btn>Lae nimed sisse</v-btn>
+    <v-btn
+      @click="loadNames"
+      :loading="nameLoading"
+    >Lae nimed sisse</v-btn>
 
     <v-file-input
       chips
       class="mt-8"
       accept=".txt"
-      label="Lae jookide fail üles(.txt fail: jook1, jook2, jook3)"
+      label="Lae jookide fail üles(.txt fail: jook1,jook2,jook3)"
     ></v-file-input>
-    <v-btn>Lae joogid sisse</v-btn>
+    <v-btn
+      @click="loadDrinks"
+      :loading="drinkLoading"
+    >Lae joogid sisse</v-btn>
 
   </v-col>
 </template>
