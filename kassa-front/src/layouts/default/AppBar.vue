848551216f7851b692735fffca4e5e26dbd1b179
inpihl <antspihl@gmail.com>
2023-06-19 16:13:30 +0300

    Header nüüd soveldaja sinine

--- kassa-front/src/layouts/default/AppBar.vue
+++ kassa-front/src/layouts/default/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app-bar flat>
-      <v-img :max-height="64" :max-width="120" src="@/assets/sov_logo.png"/>
+  <v-app-bar color="#131C43" flat>
+      <v-img class="mr-4" :max-height="64" :max-width="120" src="@/assets/sov_logo.png"/>
         <h1>Soveldaja kassa</h1>
   </v-app-bar>
 </template>
