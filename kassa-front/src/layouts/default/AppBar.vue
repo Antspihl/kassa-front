21aa5a5b43a1695e79a53fc55c846fb84f048f33
inpihl <antspihl@gmail.com>
2023-06-19 17:06:34 +0300

    Kärbitud logo

--- kassa-front/src/layouts/default/AppBar.vue
+++ kassa-front/src/layouts/default/AppBar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar color="#131C43" flat>
-      <v-img class="mr-4" :max-height="64" :max-width="120" src="@/assets/sov_logo.png"/>
+      <v-img class="mr-4 ml-4 pa-2" :max-height="64" :max-width="120" src="@/assets/sov_logo.png"/>
         <h1>Soveldaja kassa</h1>
   </v-app-bar>
 </template>
