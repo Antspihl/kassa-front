48c8c4d5cc4518bc0eba419be23c9ec0503e7cd0
inpihl <antspihl@gmail.com>
2023-06-21 17:48:17 +0300

    Lisatud vue-toastification

--- kassa-front/src/main.js
+++ kassa-front/src/main.js
@@ -6,7 +6,8 @@
 
 // Components
 import App from './App.vue'
-
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 // Composables
 import { createApp } from 'vue'
 
@@ -14,6 +15,12 @@ import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
+
+app.use(Toast, {
+  transition: "Vue-Toastification__bounce",
+  maxToasts: 10,
+  newestOnTop: true
+});
 
 registerPlugins(app)
 
