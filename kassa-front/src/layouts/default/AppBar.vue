779c3c4a5e7c77a01684c300cbbb02b36b2dd362
inpihl <antspihl@gmail.com>
2023-06-21 15:54:37 +0300

    Tekst = Baari kassa

--- kassa-front/src/layouts/default/AppBar.vue
+++ kassa-front/src/layouts/default/AppBar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar color="#131C43" flat>
       <v-img class="mr-4 ml-4 pa-2" :max-height="64" :max-width="120" src="@/assets/sov_logo.png"/>
-        <h1>Soveldaja kassa</h1>
+        <h1>Baari kassa</h1>
   </v-app-bar>
 </template>
 
