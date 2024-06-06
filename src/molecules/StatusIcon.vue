2bd4c09d9e235233c8df9bd65478fd8d1f39c360
inpihl <antspihl@gmail.com>
2024-06-06 16:15:45 +0300

    An icon component to display the type of request that is being sent

---
+++ src/molecules/StatusIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-icon
+    :color="status === 0 ? 'green' : status === 1 ? 'light-blue' : 'red'"
+    :size="24"
+  >
+    {{ status === 0 ? 'mdi-arrow-up-circle' : status === 1 ? 'mdi-swap-vertical-circle' :'mdi-close-circle'}}
+  </v-icon>
+</template>
+
+<script setup lang="ts">
+
+defineProps({
+  //0: adding, 1: changing, 2: canceling,
+  status: Number
+})
+</script>
