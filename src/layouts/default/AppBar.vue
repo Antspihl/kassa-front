725624b316cc66e989083fa259db4f6d9d80a04d
inpihl <antspihl@gmail.com>
2024-06-06 17:22:13 +0300

    Cant go to bills if there are unsent requests

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -2,7 +2,8 @@
   <v-toolbar color="primary" :flat="true">
     <v-img class="mr-4 ml-4 pa-2" :max-height="64" :max-width="120" src="@/assets/sov_logo.png"/>
     <v-btn size="x-large" :active="false" variant="text" height="64" to="/" text="Avaleht"/>
-    <v-btn size="x-large" :active="false" variant="text" height="64" to="/arved" text="Arved"/>
+    <v-btn :disabled="Object.keys(mainStore.currentRequest).length !== 0"
+           size="x-large" :active="false" variant="text" height="64" to="/arved" text="Arved"/>
     <v-spacer></v-spacer>
     <v-switch
       class=" pt-5 pl-4"
@@ -21,6 +22,9 @@
 import Settings from "@/molecules/Settings.vue";
 import {useTheme} from "vuetify";
 import {ref} from "vue";
+import {useMainStore} from "@/api/MainStore";
+
+const mainStore = useMainStore()
 
 const isLight = ref(false);
 const theme = useTheme()
